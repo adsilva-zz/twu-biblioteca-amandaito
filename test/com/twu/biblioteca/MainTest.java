b9999c30f0dc70637f83d7ba280663d643f581b7
Amanda da Silva Ito <adsilva32@gmail.com>
2020-01-16 16:46:40 -0200

    1.1 refectoring

--- test/com/twu/biblioteca/MainTest.java
+++ test/com/twu/biblioteca/MainTest.java
@@ -1,12 +1,16 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.service.MainService;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class ExampleTest {
+public class MainTest {
+
+    private MainService mainService = new MainService();
 
     @Test
     public void welcomeSuccessTest() {
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore", mainService.callWelcomeMessage());
     }
 }
